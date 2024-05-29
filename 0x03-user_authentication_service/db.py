921367d48fcd68fe9eabe60452d5515342579442
DAVID <owolagbadavid@gmail.com>
2024-05-29 18:32:39 +0100

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -55,3 +55,14 @@ class DB:
             raise NoResultFound
 
         return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user in the database
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+        return None
