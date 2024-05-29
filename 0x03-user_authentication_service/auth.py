f695d0ce1ef1bc244ce811cf9034ddbb82342160
DAVID <owolagbadavid@gmail.com>
2024-05-29 18:54:03 +0100

    flask app

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
 Auth module
 """
 
+import bcrypt
+
 
 from db import DB
 from user import User
@@ -12,8 +14,7 @@ from sqlalchemy.orm.exc import NoResultFound
 def _hash_password(password: str) -> str:
     """Hash password
     """
-    import bcrypt
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 class Auth:
@@ -24,10 +25,9 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        hashed_password = _hash_password(password)
         try:
             if self._db.find_user_by(email=email):
                 raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            pass
-        return self._db.add_user(email, hashed_password)
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
