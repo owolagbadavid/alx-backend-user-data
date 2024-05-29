5c7f19341e8b5d55e8f1b05d5d7a43a494a661b1
DAVID <owolagbadavid@gmail.com>
2024-05-29 19:01:24 +0100

    auth sh*t

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ Auth module
 """
 
 import bcrypt
+import uuid
 
 
 from db import DB
@@ -15,6 +16,12 @@ def _hash_password(password: str) -> str:
     """Hash password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generate a UUID
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -31,3 +38,19 @@ class Auth:
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password, user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
