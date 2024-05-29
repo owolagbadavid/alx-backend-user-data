85938c533b0a43950f8af012b4e2f958e0b662e6
DAVID <owolagbadavid@gmail.com>
2024-05-29 18:25:24 +0100

    find_by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -37,3 +39,19 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by a given attribute
+        """
+
+        if not kwargs:
+            raise InvalidRequestError
+        else:
+            if not all(hasattr(User, key) for key in kwargs):
+                raise InvalidRequestError
+
+        result = self._session.query(User).filter_by(**kwargs).one()
+        if result is None:
+            raise NoResultFound
+
+        return result
