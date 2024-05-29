05279202b8ed18a972f16e8bae89708de6b8768f
DAVID <owolagbadavid@gmail.com>
2024-05-29 19:03:13 +0100

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,7 +42,7 @@ class Auth:
     def valid_login(self, email: str, password: str) -> bool:
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password, user.hashed_password)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
 
