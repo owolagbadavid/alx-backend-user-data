cfa4071bce24172e63bc25336bc16882162edbd2
DAVID <owolagbadavid@gmail.com>
2024-05-30 20:48:48 +0100

    fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask app
 """
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, redirect, request, abort, make_response
 from auth import Auth
 
 AUTH = Auth()
