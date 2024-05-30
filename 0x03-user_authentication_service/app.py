547681118daa34c450c8d0e17d2fae1a744453d5
DAVID <owolagbadavid@gmail.com>
2024-05-30 20:48:22 +0100

    login/out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,5 +29,33 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """POST /sessions"""
+    user_request = request.form
+    user_email = user_request.get('email', '')
+    user_password = user_request.get('password', '')
+    valid_log = AUTH.valid_login(user_email, user_password)
+    if not valid_log:
+        abort(401)
+    response = make_response(jsonify({"email": user_email,
+                                      "message": "logged in"}))
+    response.set_cookie('session_id', AUTH.create_session(user_email))
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+    DELETE /sessions
+    """
+    user_cookie = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(user_cookie)
+    if user_cookie is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
